xiyuesaves/aseprite

commit d91c65b9ad419cad398a3c534249dd7a849c908d

Extension tests, add custom formats with saving and loading (fix #4950)

src/app/commands/cmd_save_file.cpp
@@ -20,6 +20,7 @@
 #include "app/doc.h"
 #include "app/doc_undo.h"
 #include "app/file/file.h"
+#include "app/file/file_format.h"
 #include "app/file/gif_format.h"
 #include "app/file/png_format.h"
 #include "app/file_selector.h"
@@ -36,6 +37,7 @@
 #include "app/ui/status_bar.h"
 #include "base/convert_to.h"
 #include "base/fs.h"
+#include "dio/file_format.h"
 #include "doc/mask.h"
 #include "doc/sprite.h"
 #include "doc/tag.h"
@@ -222,8 +224,14 @@ void SaveFileBaseCommand::saveDocumentInBackground(const Context* context,
   if (!fop->hasError() && resizeOnTheFly == ResizeOnTheFly::On)
     fop->setOnTheFlyScale(scale);
 
-  SaveFileJob job(fop.get(), params().ui());
-  job.showProgressWindow();
+  if (fop->fileFormat()->dioFormat() >= dio::FileFormat::FIRST_CUSTOM) {
+    // Custom formats must run on the main thread
+    fop->operate();
+  }
+  else {
+    SaveFileJob job(fop.get(), params().ui());
+    job.showProgressWindow();
+  }
 
   if (fop->hasError()) {
     Console console;

cmake/FindTests.cmake
@@ -15,6 +15,9 @@ function(find_tests dir dependencies)
   foreach(testsourcefile ${tests})
     get_filename_component(testname ${testsourcefile} NAME_WE)
 
+    # Necessary for Extensions tests on Windows
+    add_definitions(-DLIBARCHIVE_STATIC)
+
     add_executable(${testname} ${testsourcefile})
     add_test(NAME ${testname} COMMAND ${testname})
 

data/strings/en.ini
@@ -71,8 +71,8 @@ error_loading_file = Error<<Error loading file: {0}||&OK
 error_saving_file = Error<<Error saving file: {0}||&OK
 file_format_doesnt_support_palette = Error<<Cannot save a color palette in {0} format||&OK
 export_animation_in_sequence = Notice\n<<Do you want to export the animation in {0} files?\n<<{1}, {2}...\n||&Agree||&Cancel
-file_format_doesnt_support_error = Error\n<<File format ".{0}" doesn't support:\n<<\n{1}\n<<\n<<You must select other format.\n<<Use ".aseprite" to keep all the sprite information.\n||&OK
-file_format_doesnt_support_warning = Warning\n<<File format ".{0}" doesn't support:\n<<\n{1}\n<<\n<<You can use ".aseprite" format to keep all this information.\n<<Do you want continue with ".{0}" anyway?\n||&Yes||&No
+file_format_doesnt_support_error = Error\n<<File format "{0}" (.{1}) doesn't support:\n<<\n{2}\n<<\n<<You must select another format.\n<<Use ".aseprite" to keep all the sprite information.\n||&OK
+file_format_doesnt_support_warning = Warning\n<<File format "{0}" (.{1}) doesn't support:\n<<\n{2}\n<<\n<<You can use ".aseprite" format to keep all this information.\n<<Do you want continue with "{0}" anyway?\n||&Yes||&No
 file_format_alpha_channel = Alpha channel
 file_format_tags = Tags
 file_format_frames = Frames

src/app/commands/cmd_open_file.cpp
@@ -17,6 +17,7 @@
 #include "app/console.h"
 #include "app/doc.h"
 #include "app/file/file.h"
+#include "app/file/file_format.h"
 #include "app/file_selector.h"
 #include "app/i18n/strings.h"
 #include "app/pref/preferences.h"
@@ -25,6 +26,7 @@
 #include "app/ui_context.h"
 #include "app/util/open_file_job.h"
 #include "base/fs.h"
+#include "dio/file_format.h"
 #include "doc/sprite.h"
 #include "ui/ui.h"
 
@@ -170,8 +172,14 @@ void OpenFileCommand::onExecute(Context* context)
         m_usedFiles.push_back(fn);
       }
 
-      OpenFileJob task(fop.get(), m_ui);
-      task.showProgressWindow();
+      if (fop->fileFormat()->dioFormat() >= dio::FileFormat::FIRST_CUSTOM) {
+        // Custom formats must run on the main thread
+        fop->operate();
+      }
+      else {
+        OpenFileJob task(fop.get(), m_ui);
+        task.showProgressWindow();
+      }
 
       // Post-load processing, it is called from the GUI because may require user intervention.
       fop->postLoad();

src/app/commands/cmd_options.cpp
@@ -273,7 +273,7 @@ class OptionsWindow : public app::gen::Options {
     {
       ASSERT(m_extension);
       ASSERT(canBeUninstalled());
-      App::instance()->extensions().uninstallExtension(m_extension, DeletePluginPref::kYes);
+      App::instance()->extensions().uninstallExtension(m_extension, DeletePluginPref::Yes);
       m_extension = nullptr;
     }
 
@@ -1320,7 +1320,7 @@ private:
 
       for (auto* e : uninstall) {
         try {
-          App::instance()->extensions().uninstallExtension(e, DeletePluginPref::kYes);
+          App::instance()->extensions().uninstallExtension(e, DeletePluginPref::Yes);
         }
         catch (const std::exception& ex) {
           LOG(ERROR,
@@ -2049,7 +2049,7 @@ private:
 
         // Uninstall old version
         if (ext->canBeUninstalled()) {
-          exts.uninstallExtension(ext, DeletePluginPref::kNo);
+          exts.uninstallExtension(ext, DeletePluginPref::No);
 
           ExtensionItem* item = getItemByExtension(ext);
           if (item)

src/app/commands/params.h
@@ -7,6 +7,7 @@
 #ifndef APP_COMMANDS_PARAMS_H
 #define APP_COMMANDS_PARAMS_H
 
+#include <initializer_list>
 #include <map>
 #include <sstream>
 #include <string>
@@ -18,6 +19,9 @@ public:
   typedef std::map<std::string, std::string> Map;
   typedef Map::iterator iterator;
   typedef Map::const_iterator const_iterator;
+
+  Params() = default;
+  Params(std::initializer_list<Map::value_type> params) : m_params(params) {}
 
   iterator begin() { return m_params.begin(); }
   iterator end() { return m_params.end(); }

src/app/doc.cpp
@@ -175,6 +175,12 @@ DocApi Doc::getApi(Transaction& transaction)
 //////////////////////////////////////////////////////////////////////
 // Main properties
 
+void Doc::resetUndoHistory()
+{
+  m_undo = std::make_unique<DocUndo>();
+  m_undo->setContext(m_ctx);
+}
+
 bool Doc::isUndoing() const
 {
   return m_undo->isUndoing();

src/app/doc.h
@@ -101,6 +101,7 @@ public:
 
   const DocUndo* undoHistory() const { return m_undo.get(); }
   DocUndo* undoHistory() { return m_undo.get(); }
+  void resetUndoHistory();
 
   bool isUndoing() const;
 

commit 8974b62ced0c1edc25a801f81f558bfa2487c4bc

Fix crash using an invalid file format extension in File > Save (fix #5865)
Regression introduced in d91c65b9ad419cad398a3c534249dd7a849c908d

src/app/commands/cmd_save_file.cpp
@@ -221,16 +221,18 @@ void SaveFileBaseCommand::saveDocumentInBackground(const Context* context,
   if (!fop)
     return;
 
-  if (!fop->hasError() && resizeOnTheFly == ResizeOnTheFly::On)
-    fop->setOnTheFlyScale(scale);
+  if (!fop->hasError()) {
+    if (resizeOnTheFly == ResizeOnTheFly::On)
+      fop->setOnTheFlyScale(scale);
 
-  if (fop->fileFormat()->dioFormat() >= dio::FileFormat::FIRST_CUSTOM) {
-    // Custom formats must run on the main thread
-    fop->operate();
-  }
-  else {
-    SaveFileJob job(fop.get(), params().ui());
-    job.showProgressWindow();
+    if (fop->fileFormat() && fop->fileFormat()->dioFormat() >= dio::FileFormat::FIRST_CUSTOM) {
+      // Custom formats must run on the main thread
+      fop->operate();
+    }
+    else {
+      SaveFileJob job(fop.get(), params().ui());
+      job.showProgressWindow();
+    }
   }
 
   if (fop->hasError()) {